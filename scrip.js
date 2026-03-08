5a3fb062a253de376d255e251f44988cd1751e9f
JoseRGomezR05 <jose.gomez.ttc.688@unilibre.edu.co>
2026-03-07 22:10:21 -0500

    h

--- scrip.js
+++ scrip.js
@@ -1,35 +1,23 @@
 function calcularEdad(){
-
 let nacimiento = new Date("1990-08-05")
 let hoy = new Date()
-
 let edad = hoy.getFullYear() - nacimiento.getFullYear()
-
 let m = hoy.getMonth() - nacimiento.getMonth()
 
 if(m < 0 || (m === 0 && hoy.getDate() < nacimiento.getDate())){
-
 edad--
-
 }
-
 document.getElementById("edad").innerText = edad
-
 }
 
 calcularEdad()
 
-
-
 function sendLocation(){
 
 if(navigator.geolocation){
-
 navigator.geolocation.getCurrentPosition(function(position){
-
 let lat = position.coords.latitude
 let lon = position.coords.longitude
-
 let url = "https://www.google.com/maps?q="+lat+","+lon
 
 alert("Ubicación generada:\n"+url)
@@ -43,8 +31,6 @@ alert("Geolocalización no soportada")
 }
 
 }
-
-
 
 function setLanguage(lang){
 
@@ -60,7 +46,6 @@ document.getElementById("alerta").innerText="⚠️ ALLERGIC TO NSAIDs"
 }
 
 if(lang === "es"){
-
 document.getElementById("birth_label").innerText="Fecha nacimiento:"
 document.getElementById("age_label").innerText="Edad:"
 document.getElementById("id_label").innerText="Cédula:"
@@ -69,5 +54,4 @@ document.getElementById("contact_label").innerText="Contacto de Emergencia"
 document.getElementById("alerta").innerText="⚠️ ALÉRGICO A LOS AINES"
 
 }
-
 }
